48e37e63aae5414fe7b26288b17d948853a5b699
dhindonk <mfahdin1803@gmail.com>
2024-01-26 13:43:27 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:intl/intl.dart';
@@ -36,6 +35,9 @@ class _ImagePickersState extends State<ImagePickers> {
   late XFile image;
   late Position? currentPosition;
   String? addressFull;
+  String? latLong;
+  String? date;
+  bool one = false;
 
   void imgFromCamera() async {
     XFile? pickedImg = await ImagePicker().pickImage(
@@ -48,7 +50,6 @@ class _ImagePickersState extends State<ImagePickers> {
         image = pickedImg;
         print('INI PATH NYA : ${image.path}');
       });
-      print('INI LOKASI ke2 : ');
       getLocation();
     }
   }
@@ -60,99 +61,83 @@ class _ImagePickersState extends State<ImagePickers> {
   }
 
   Future<void> mintaAkses() async {
-    LocationPermission permission;
-    permission = await Geolocator.requestPermission();
+    await Geolocator.requestPermission();
   }
 
-//   Future<void> getAddress() async {
-//     List<Placemark> placemarks =
-//         await placemarkFromCoordinates(-6.5993931,106.8097919);
+  bool isLoading = false;
 
-//     setState(() {
-//       addressFull = placemarks.last.name;
-// // subLocality,locality,subAdministrativeArea , administrativeArea, postalCode
-
-//       // addressFull = placemarks
-//       //     .map((placemark) =>
-//       //         "${placemark.country}, ${placemark.subLocality}, ${placemark.locality}, ${placemark.subAdministrativeArea}, ${placemark.administrativeArea} ${placemark.postalCode}, ${placemark.country}")
-//       //     .join(", ");
-//     });
-//   }
-
-Future<void> getAddress(double latitude, double longitude) async {
-  List<Placemark> placemarks = await placemarkFromCoordinates(latitude, longitude);
-
-  if (placemarks != null && placemarks.isNotEmpty) {
-    Placemark placemark = placemarks.first; 
-
-    
-    String? street = placemark.name; // Jl. Pakuan
-    String? subLocality = placemark.subLocality; // Tegallega
-    String? locality = placemark.locality; // Kota Bogor
-    String? subAdminArea = placemark.subAdministrativeArea; // Kecamatan Bogor Tengah
-    String? postalCode = placemark.postalCode; // 16129
-    String? administrativeArea = placemark.administrativeArea; // Jawa Barat
-    String name = placemark.name!; //
-
-    // Mencari RT/RW
-    RegExp rtRwRegex = RegExp(r'RT\.(\d+)\/RW\.(\d+)');
-    Match? rtRwMatch = rtRwRegex.firstMatch(name);
-    String rt = rtRwMatch != null ? 'RT.${rtRwMatch.group(1)}/' : '';
-    String rw = rtRwMatch != null ? 'RW.${rtRwMatch.group(2)}' : '';
-
-   
-    String addressFulls = '$street, $rt/$rw, $subLocality, $subAdminArea, $locality, $administrativeArea $postalCode';
-
+  Future<void> getLocation() async {
     setState(() {
-      addressFull = addressFulls;
+      isLoading = !isLoading;
     });
-  } else {
-    setState(() {
-      addressFull = "Alamat tidak ditemukan";
-    });
-  }
-}
-
-  
-
-  void getLocation() async {
     Position position = await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.high,
     );
 
     var tanggalSekarang =
         DateFormat.yMMMMEEEEd().format(DateTime.now().toLocal());
-    var waktuSekarang = DateFormat.jms().format(DateTime.now().toLocal());
+    var waktuSekarang = DateFormat('HH:mm:ss').format(DateTime.now().toLocal());
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(-4.4992317,105.2413371);
 
-    print('LATITUDE : ${position.latitude}');
-    print('LONGTITUDE : ${position.longitude}');
-    print('Akurasi : ${position.accuracy}');
-    print('Heading : ${position.heading}');
-    print('TIMESTAMP : ${tanggalSekarang} - ${waktuSekarang} ');
-    print('Full Address : ');
-    print('altitude : ${position.altitude}');
-    print('ISMOCKED : ${position.isMocked}');
-    print('speed  : ${position.speed}');
-    print('speed akurasi : ${position.speedAccuracy}');
+    if (placemarks.isNotEmpty) {
+      Placemark placemark = placemarks.first;
+
+      String address11 = placemark.thoroughfare ?? ''; // Nama jalan
+      String address21 = placemark.subThoroughfare ?? ''; // Nama jalan
+      String address31 = placemark.street ?? ''; // Nama jalan
+      
+      String subLocality = placemark.subLocality ?? ''; // Kelurahan
+      String locality = placemark.locality ?? ''; // Kota
+      String subAdminArea = placemark.subAdministrativeArea ?? ''; // Kecamatan
+      String postalCode = placemark.postalCode ?? ''; // Kode pos
+      String administrativeArea =
+          placemark.administrativeArea ?? ''; // Provinsi
+      String country = placemark.country ?? ''; // Negara
+
+      String fullAddress =
+          '\n First : $address11 || $address21 || $address31   \n $subLocality, $locality, $subAdminArea, $administrativeArea $postalCode, $country';
+      String dateNew = '$tanggalSekarang - $waktuSekarang';
+
+      setState(() {
+        one = !one;
+        addressFull = fullAddress;
+        date = dateNew;
+        isLoading = !isLoading;
+        latLong =
+            'Latitude : ${position.latitude} \n Long : ${position.longitude}';
+      });
+    } else {
+      setState(() {
+        addressFull = "Alamat tidak ditemukan";
+      });
+    }
+
+    // print('LATITUDE : ${position.latitude}');
+    // print('LONGTITUDE : ${position.longitude}');
+    // print('Akurasi : ${position.accuracy}');
+    // print('Heading : ${position.heading}');
+    // print('TIMESTAMP : ${tanggalSekarang} - ${waktuSekarang} ');
+    // print('Full Address : ');
+    // print('altitude : ${position.altitude}');
+    // print('ISMOCKED : ${position.isMocked}');
+    // print('speed  : ${position.speed}');
+    // print('speed akurasi : ${position.speedAccuracy}');
   }
   //
   // https://medium.com/@fernnandoptr/how-to-get-users-current-location-address-in-flutter-geolocator-geocoding-be563ad6f66a
 
   // try {
-
   //   // Mendapatkan alamat lengkap
   //   String fullAddress = placemarks
   //       .map((placemark) =>
   //           "${placemark.name}, ${placemark.subLocality}, ${placemark.locality}, ${placemark.subAdministrativeArea}, ${placemark.administrativeArea} ${placemark.postalCode}, ${placemark.country}")
   //       .join(", ");
-
   // } catch (e) {
   //   print("Error: $e");
   // }
-
   // getUserLocation() async {
   //   //call this async method from whereever you need
-
   //   LocationData myLocation;
   //   String error;
   //   Location location = new Location();
@@ -189,22 +174,40 @@ Future<void> getAddress(double latitude, double longitude) async {
           children: [
             ElevatedButton(
               onPressed: () => imgFromCamera(),
-              child: Text('Klik'),
+              child: const Text('Take Picture'),
             ),
-            SizedBox(
+            const SizedBox(
               height: 50,
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text('Full Alamat : ${addressFull}'),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Date : $date'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Full Alamat : $addressFull'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(latLong.toString()),
+                ),
+              ],
             ),
-            SizedBox(
+            const SizedBox(
               height: 50,
             ),
-            ElevatedButton(
-              onPressed: () => getAddress(-6.5993931,106.8097919),
-              child: Text('Cek COntary'),
-            ),
+            isLoading == true
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : ElevatedButton(
+                    onPressed: one == false ? () => getLocation() : () {},
+                    child: one == false
+                        ? const Text('Cek COntary')
+                        : const Text("CAN'T KLIK"),
+                  ),
           ],
         ),
       ),
